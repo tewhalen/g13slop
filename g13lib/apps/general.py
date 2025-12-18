66953915b5b9074cafe5f91cf6092bfbb4063406
ted whalen <tewhalen@gmail.com>
2025-12-18 10:02:12 -0600

    Refactor GeneralManager to reuse LCDCompositor instance
    GeneralManager now initializes a single LCDCompositor instance in its constructor and reuses it, instead of creating a new instance each time the compositor() method is called. This improves efficiency and ensures consistent state across uses.

--- g13lib/apps/general.py
+++ g13lib/apps/general.py
@@ -6,14 +6,16 @@ from g13lib.render_fb import LCDCompositor
 
 
 class GeneralManager(InputManager):
+    _compositor: LCDCompositor
+
     def __init__(self):
         super().__init__()
-
+        self._compositor = LCDCompositor(LogEmulator())
         blinker.signal("release_focus").connect(self.activate)
         blinker.signal("single_focus").connect(self.deactivate)
 
     def compositor(self):
-        return LCDCompositor(LogEmulator())
+        return self._compositor
 
     def activate(self, msg):
         blinker.signal("set_compositor").send(self.compositor())
